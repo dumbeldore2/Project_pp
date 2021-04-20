5190a5e20ca15494f43c3313fced79de9529b1e2
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-04-20 23:30:58 +0200

    de layout van MainActivity5.java is volledig correct

--- app/src/main/java/com/example/project_pp/MainActivity5.java
+++ app/src/main/java/com/example/project_pp/MainActivity5.java
@@ -1,14 +1,56 @@
 package com.example.project_pp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity5 extends AppCompatActivity {
 
+    //tittel opvragen om kleur te geven
+    TextView textView1;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+        //dit zorgt ervoor dat deze app word gedraaid in een fullscreen
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        //deze gaat de topbar omzetten in een kleur , die doet dit ook met de bot bar
+        getWindow().setNavigationBarColor(Color.parseColor("#ffffff"));
+        getWindow().setStatusBarColor(Color.parseColor("#ffffff"));
+
+        //texvieuw aan een object vast hangen
+        textView1 = findViewById(R.id.text1);
+
+        //textvieuw kleuren
+        setColorToGradiant(textView1);
+
     }
+
+
+    //de functie van de verf lol
+    private void setColorToGradiant(TextView textView){
+        TextPaint textPaint = textView.getPaint();
+        float width = textPaint.measureText(textView.getText().toString());
+
+        Shader shader = new LinearGradient(width/2,0,width/2,textView.getTextSize(),
+                new int[]{
+                        Color.parseColor("#00BF8F"),
+                        Color.parseColor("#001510")
+                },null,Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(shader);
+    }
+
 }

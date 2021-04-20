4076dfa0b03151a77db32e93e610660439bcb9a0
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-04-20 23:19:31 +0200

    de add website knop op MainActivity4.java werkt en gaat terug naar de MainActivity2.java, deze checkt wel nog niet of dat deze is deftig ingevuld of niet

--- app/src/main/java/com/example/project_pp/MainActivity4.java
+++ app/src/main/java/com/example/project_pp/MainActivity4.java
@@ -1,14 +1,71 @@
 package com.example.project_pp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    //tittel opvragen om kleur te geven
+    TextView textView1,textView3;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        //dit zorgt ervoor dat deze app word gedraaid in een fullscreen
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        //deze gaat de topbar omzetten in een kleur , die doet dit ook met de bot bar
+        getWindow().setNavigationBarColor(Color.parseColor("#ffffff"));
+        getWindow().setStatusBarColor(Color.parseColor("#ffffff"));
+
+
+        //texvieuw aan een object vast hangen
+        textView1 = findViewById(R.id.text1);
+        textView3 = findViewById(R.id.text3);
+
+        //textvieuw kleuren
+        setColorToGradiant(textView1);
+
+        //functies
+        clickOnAdd();
+    }
+
+    //de functie van de verf lol
+    private void setColorToGradiant(TextView textView){
+        TextPaint textPaint = textView.getPaint();
+        float width = textPaint.measureText(textView.getText().toString());
+
+        Shader shader = new LinearGradient(width/2,0,width/2,textView.getTextSize(),
+                new int[]{
+                        Color.parseColor("#00BF8F"),
+                        Color.parseColor("#001510")
+                },null,Shader.TileMode.CLAMP);
+        textView.getPaint().setShader(shader);
+    }
+
+    //de functie clickOnAdd
+    public void clickOnAdd(){
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
